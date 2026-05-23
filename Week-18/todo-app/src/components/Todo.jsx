8d88612a24a84953bb609001b67ab13f4ac335f4
Anupam gupta <c2gupt@gmail.com>
2026-05-23 23:00:01 +0530

    feat: implement todo functionality with add, edit, and delete features

--- Week-18/todo-app/src/components/Todo.jsx
+++ Week-18/todo-app/src/components/Todo.jsx
@@ -1,13 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Todo() {
-  const [todo, setTodo] = useState();
-  
+  const [todo, setTodo] = useState("");
+
+  const [todoarr, settodoarr] = useState([]);
+
+  const submitTodo = (e) => {
+    e.preventDefault();
+
+    
+
+    settodoarr([
+      ...todoarr,
+      {
+        id: Date.now(),
+        todo: todo,
+      },
+    ]);
+    setTodo("");
+  };
+
+  const editHandel = (id) => {
+
+    const edit = todoarr.find((item) => item.id === id);
+    console.log(edit);
+    setTodo(edit.todo);
+
+
+  };
+
+  const DeletHandel = (id) => {
+    console.log(id);
+
+    const onlyMatched = todoarr.filter((item) => item.id !== id);
+    settodoarr(onlyMatched);
+  };
+
+  console.log(todoarr);
+
   return (
-    <div>
-      <input type="text" />
-      <button>Add</button>
-    </div>
+    <>
+      <div>
+        <input
+          type="text"
+          name="todo"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button onClick={submitTodo}>Add</button>
+      </div>
+
+      <div className="bg-red-500   ">
+        <ul>
+          {todoarr.map((todo) => {
+            return (
+              <li key={todo.id} className="flex">
+                {todo.todo}
+
+                <button
+                  onClick={() => {
+                    editHandel(todo.id);
+                  }}
+                >
+                  edit
+                </button>
+                <button
+                  onClick={() => {
+                    DeletHandel(todo.id);
+                  }}
+                >
+                  delet
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
   );
 }
 
