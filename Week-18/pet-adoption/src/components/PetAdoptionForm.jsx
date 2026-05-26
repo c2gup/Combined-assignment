20528ce3a8c41667425258e8ed99c8e6c0a87d24
Anupam gupta <c2gupt@gmail.com>
2026-05-27 04:51:20 +0530

    feat: implement pet adoption form with user input handling and data display

--- Week-18/pet-adoption/src/components/PetAdoptionForm.jsx
+++ Week-18/pet-adoption/src/components/PetAdoptionForm.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import React, { useState } from "react";
+import AdopterData from "./AdopterData";
 
 const PetAdoptionForm = () => {
-  return (
-    <div>PetAdoptionForm</div>
-  )
-}
+  const [formData, setFormData] = useState([]);
+  const [petName, setpetName] = useState("");
+  const [petType, setpetType] = useState("");
+  const [yourName, setyourName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
 
-export default PetAdoptionForm
+  const [showForm, setShowFrom] = useState(false);
+  const handleGoBack = () => {
+    setShowFrom(!true);
+  };
+  const handelForm = (e) => {
+    e.preventDefault();
+
+    setFormData((prev) => {
+      return [
+        ...prev,
+        {
+          petName,
+          petType,
+          yourName,
+          email,
+          phone,
+        },
+      ];
+    });
+
+    setShowFrom(true);
+  };
+
+  console.log(formData);
+
+  if (!showForm) {
+    return (
+      <form onSubmit={handelForm} className="form">
+        <label htmlFor="">Pet Name</label>
+        <input
+          type="text"
+          placeholder="Pet Name"
+          value={petName}
+          onChange={(e) => setpetName(e.target.value)}
+        />
+
+        <label htmlFor="">Pet Type</label>
+        <input
+          type="text"
+          placeholder="Pet Type"
+          value={petType}
+          onChange={(e) => setpetType(e.target.value)}
+        />
+
+        <label htmlFor="">Your Name</label>
+        <input
+          type="text"
+          placeholder="Pet Name"
+          value={yourName}
+          onChange={(e) => setyourName(e.target.value)}
+          name="PetName"
+        />
+
+        <label htmlFor="">Email</label>
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="">Phone</label>
+        <input
+          type="text"
+          name="phone"
+          placeholder="Phone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
+
+        <button>Submit</button>
+      </form>
+    );
+  }
+
+  return (
+    <div>
+      <AdopterData formData={formData} handleGoBack={handleGoBack} />
+    </div>
+  );
+};
+
+export default PetAdoptionForm;
