01b1ece81c808b401f7d732a9f3613dfcf74f150
Anupam gupta <c2gupt@gmail.com>
2026-05-29 15:56:24 +0530

    feat: implement file writing functionality and create initial myfile.txt

--- Week-02/01-async-js/easy/4-write-to-file.js
+++ Week-02/01-async-js/easy/4-write-to-file.js
@@ -2,3 +2,19 @@
 
 // Using the fs library again, try to write to the contents of a file.
 // You can use the fs library to as a black box, the goal is to understand async tasks.
+
+const fs = require("fs").promises;
+
+const write = async () => {
+  try {
+    await fs.writeFile("myfile.txt", "Hello, World!", "utf8");
+    const data = { name: "John", age: 30, city: "New York" };
+    await fs.writeFile("data.json", JSON.stringify(data, null, 2), "utf8");
+
+    console.log("Files created successfully");
+  } catch (error) {
+    console.error("Error writing files:", err);
+  }
+};
+
+write();
