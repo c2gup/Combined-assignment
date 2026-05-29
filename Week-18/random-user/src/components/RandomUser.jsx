13b3f9886227a529033ff375ae9d0b8f3d8ceea1
Anupam gupta <c2gupt@gmail.com>
2026-05-29 17:15:57 +0530

    fix: correct loading message typo and improve code formatting in RandomUser component

--- Week-18/random-user/src/components/RandomUser.jsx
+++ Week-18/random-user/src/components/RandomUser.jsx
@@ -1,12 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './RandomUser.css'; // Import the external CSS file
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./RandomUser.css"; // Import the external CSS file
 
 const RandomUser = () => {
-  
+  const [userData, setUserData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const fetchData = async () => {
+    try {
+      const data = await axios.get("https://randomuser.me/api/");
+
+      setUserData(data.data?.results);
+      setLoading(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log("-------->", userData);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="random-user-container">
-     <h1>RandomUser</h1>
+      <h1>RandomUser</h1>
+      {loading ? (
+        <>
+          {userData.map((user, index) => {
+            return (
+              <>
+                <div key={index}>
+                  <img src={user.picture.medium} alt="" />
+
+                  <h1>{`${user.name.first} ${user.name.last}`}</h1>
+                  <p>{user.email}</p>
+                </div>
+              </>
+            );
+          })}
+        </>
+      ) : (
+        <p>Lodading</p>
+      )}
     </div>
   );
 };
