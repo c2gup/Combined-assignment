0c958e59ca51e40b6247ee06a8642f7ea20346a3
Anupam gupta <c2gupt@gmail.com>
2026-05-29 15:40:51 +0530

    refactor: enhance file reading example with synchronous and asynchronous methods

--- Week-02/01-async-js/easy/3-read-from-file.js
+++ Week-02/01-async-js/easy/3-read-from-file.js
@@ -1,7 +1,30 @@
 // ## Reading the contents of a file
 
-// Write code to read contents of a file and print it to the console. 
-// You can use the fs library to as a black box, the goal is to understand async tasks. 
-// Try to do an expensive operation below the file read and see how it affects the output. 
-// Make the expensive operation more and more expensive and see how it affects the output. 
+// Write code to read contents of a file and print it to the console.
+// You can use the fs library to as a black box, the goal is to understand async tasks.
+// Try to do an expensive operation below the file read and see how it affects the output.
+// Make the expensive operation more and more expensive and see how it affects the output.
 
+const fs = require("fs");
+
+try {
+  const data = fs.readFileSync("myfile.txt", "utf8");
+  console.log(data);
+} catch (err) {
+  console.log(err);
+}
+
+console.log("--------------------------");
+
+fs.readFile("myfile.txt", "utf8", (err, data) => {
+  console.log("File content:", data);
+});
+
+// Expensive operation
+let sum = 0;
+
+for (let i = 0; i < 10000000000; i++) {
+  sum += i;
+}
+
+console.log("Expensive operation done");
