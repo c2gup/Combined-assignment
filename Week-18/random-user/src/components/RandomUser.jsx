d6fc3e742ca81faeedf80c6847b07dc548f2bea7
Anupam gupta <c2gupt@gmail.com>
2026-05-29 19:48:30 +0530

    feat: add RandomUser component with pagination and improve loading state handling

--- Week-18/random-user/src/components/RandomUser.jsx
+++ Week-18/random-user/src/components/RandomUser.jsx
@@ -5,12 +5,16 @@ import "./RandomUser.css"; // Import the external CSS file
 const RandomUser = () => {
   const [userData, setUserData] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
   const fetchData = async () => {
+    setLoading(true);
     try {
-      const data = await axios.get("https://randomuser.me/api/");
+      const response = await axios.get(
+        `https://randomuser.me/api/?page=${page}&results=5`,
+      );
+      setUserData((prevUsers) => [...prevUsers, ...response.data.results]);
 
-      setUserData(data.data?.results);
-      setLoading(true);
+      setLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -20,28 +24,36 @@ const RandomUser = () => {
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [page]);
+
+  const handelMore = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
 
   return (
     <div className="random-user-container">
-      <h1>RandomUser</h1>
-      {loading ? (
+      <h1>Random Users</h1>
+      {!loading ? (
         <>
-          {userData.map((user, index) => {
+          {userData?.map((user, index) => {
             return (
-              <>
+              <div className=" users-list">
                 <div key={index}>
                   <img src={user.picture.medium} alt="" />
 
                   <h1>{`${user.name.first} ${user.name.last}`}</h1>
                   <p>{user.email}</p>
                 </div>
-              </>
+              </div>
             );
           })}
+
+          <div>
+            <button onClick={handelMore}>Load More Users</button>
+          </div>
         </>
       ) : (
-        <p>Lodading</p>
+        <p>Loading...</p>
       )}
     </div>
   );
